f0af89e66dcef44f38ea6a78ac00e92b081a045a
jqnfxa <ivborisov@stud.etu.ru>
2024-11-02 04:30:48 +0300

    update: example of parser usage

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@
 int main()
 {
 	std::string is;
-	std::cin >> is;
-
-	auto target_expression = ExpressionParser(is).parse();
-	std::cout << "your expression: " << target_expression << "\n";
-
+	//std::cin >> is;
+	Expression axiom1 = ExpressionParser("a>(b>a)").parse();
+	Expression axiom2 = ExpressionParser("(a>(b>c))>((b>c)>(a>c))").parse();
+	Expression a = ExpressionParser("a").parse();
+	std::cout << axiom1 << '\n';
+	axiom2.replace(3, a);
+	std::cout << axiom2 << '\n';
+	std::cout << axiom2.subtree(axiom2.right(0));
 	return 0;
 }
