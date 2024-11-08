4fa0482b75e6c4aedf18388454e8ead23b24e95f
jqnfxa <ivborisov@stud.etu.ru>
2024-11-08 03:28:17 +0300

    refactor: axiom 11 example

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "./math/ast.hpp"
-#include "./parser/parser.hpp"
+#include "./math/rules.hpp"
 
 
 int main()
 {
-	Expression axiom1("(!a>(b>c))>((b>c)>(!a>c))");
-	Expression axiom2("(a>(b>a))");
-	axiom1.replace(1, axiom2);
-	std::cout << axiom1.to_string() << std::endl;
+	Expression axiom1("(a>(b>a))");
+	Expression axiom2("(a>(b>c))>((a>b)>(a>c))");
+
+	Expression e1 = modus_ponens(axiom1, axiom2);
+	Expression e2 = modus_ponens(axiom1, e1);
+
+	std::cout << e1.to_string() << '\n';
+	std::cout << e2.to_string() << '\n';
 	return 0;
 }
