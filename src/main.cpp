0bec2036f28d37e5402fc51aa2838d3255cf12f7
jqnfxa <ivborisov@stud.etu.ru>
2024-11-14 02:10:04 +0300

    small corrections

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 
 int main()
 {
-	/* implication swap rule: (a>!b)>(b>!a)
-	Expression ax1("a>(b>a)");
+	// (isr) implication swap rule: (!a>!b)>(b>a)
+	/*Expression ax1("a>(b>a)");
 	Expression ax2("(a>(b>c))>((a>b)>(a>c))");
 	Expression ax3("(!a>!b)>((!a>b)>a)");
 	std::vector<Expression> axioms = { ax1, ax2, ax3 };
@@ -23,18 +23,13 @@ int main()
 	axioms.push_back(modus_ponens(axioms[7], axioms[8]));
 	axioms.push_back(modus_ponens(axioms[3], axioms[9]));
 
-	for (const auto &axiom : axioms)
+	for (std::size_t i = 0; i < axioms.size(); ++i)
 	{
-		std::cout << axiom << '\n';
-	}
-	*/
+		std::cout << i << ' ' << axioms[i] << '\n';
+	}*/
 
-	/*
-	ax4 (a*b>a) ~ !a>!(a*b) ~ !a>(a>!b) (ax10)
-	*/
-
-	/* ax5 (a*b>b)
-	Expression ax1("a>(b>a)");
+	// ax5 (a*b>b)
+	/*Expression ax1("a>(b>a)");
 	Expression ax2("(a>(b>c))>((a>b)>(a>c))");
 	Expression ax3("(!a>!b)>((!a>b)>a)");
 	std::vector<Expression> axioms = { ax1, ax2, ax3 };
@@ -50,14 +45,13 @@ int main()
 	for (std::size_t i = 0; i < axioms.size(); ++i)
 	{
 		std::cout << i << ' ' << axioms[i] << '\n';
-	}
-	*/
+	}*/
 
-	/* ax11 (a>a)
+	// ax11 (a>a)
+	/*
 	Expression ax1("a>(b>a)");
 	Expression ax2("(a>(b>c))>((a>b)>(a>c))");
-	Expression ax3("(!a>!b)>((!a>b)>a)");
-	std::vector<Expression> axioms = { ax1, ax2, ax3 };
+	std::vector<Expression> axioms = { ax1, ax2 };
 
 	axioms.push_back(modus_ponens(axioms[0], axioms[1]));
 	axioms.push_back(modus_ponens(axioms[0], axioms[2]));
@@ -68,18 +62,20 @@ int main()
 	}
 	*/
 
-	/* ax7 (a>(!a>b)))
+	// ax7 (a>(!a>b)))
+	/*
 	Expression ax1("a>(b>a)");
 	Expression ax2("(a>(b>c))>((a>b)>(a>c))");
 	Expression ax3("(!a>!b)>((!a>b)>a)");
-	Expression ax_swap_rule("(!a>!b)>(b>a)");
-	Expression ax5("(!a>!b)>(b>a)");
-	std::vector<Expression> axioms = { ax1, ax2, ax3, ax_swap_rule, ax5 };
+	Expression isr("(!a>!b)>(b>a)");
+	std::vector<Expression> axioms = { ax1, ax2, ax3, isr };
 
-	axioms.push_back(modus_ponens(axioms[2], axioms[0]));
-	axioms.push_back(modus_ponens(axioms[3], axioms[2]));
-	axioms.push_back(modus_ponens(axioms[4], axioms[1]));
-	axioms.push_back(modus_ponens(axioms[5], axioms[6]));
+	axioms.push_back(modus_ponens(axioms[0], axioms[0]));
+	axioms.push_back(modus_ponens(axioms[3], axioms[0]));
+	axioms.push_back(modus_ponens(axioms[1], axioms[1]));
+	axioms.push_back(modus_ponens(axioms[2], axioms[1]));
+	axioms.push_back(modus_ponens(axioms[5], axioms[1]));
+	axioms.push_back(modus_ponens(axioms[0], axioms[8]));
 
 	for (std::size_t i = 0; i < axioms.size(); ++i)
 	{
@@ -87,7 +83,8 @@ int main()
 	}
 	*/
 
-	/* ax8 (b>(!a>b)))
+	// ax8 (b>(!a>b)))
+	/*
 	Expression ax1("a>(b>a)");
 	std::vector<Expression> axioms = { ax1 };
 
@@ -97,14 +94,15 @@ int main()
 	}
 	*/
 
-	/* ax10 (!a>(a>b)))
+	// ax10 (!a>(a>b)))
+	/*
 	Expression ax1("a>(b>a)");
 	Expression ax2("(a>(b>c))>((a>b)>(a>c))");
 	Expression ax_swap_rule("(!a>!b)>(b>a)");
 	std::vector<Expression> axioms = { ax1, ax2, ax_swap_rule };
 
 	axioms.push_back(modus_ponens(axioms[2], axioms[0]));
-	axioms.push_back(modus_ponens(axioms[3], axioms[2]));
+	axioms.push_back(modus_ponens(axioms[3], axioms[1]));
 	axioms.push_back(modus_ponens(axioms[0], axioms[4]));
 
 	for (std::size_t i = 0; i < axioms.size(); ++i)
@@ -113,12 +111,22 @@ int main()
 	}
 	*/
 
-	/* ax6 (a>(b>!(a->!b)))
-	Expression ax1("a>(b>a)");
+	// ax4 (a*b>a)
+	/*
+	Expression ax10("!a>(a>b)");
+	Expression isr("(!a>!b)>(b>a)");
+	Expression guess("a*b>a");
+	guess.make_permanent();
+
+	mp(ax4, isr) = P>(!P>!Q) ax10
+	*/
+
+	// ax6 (a>(b>(a*b)))
+	/*Expression ax1("a>(b>a)");
 	Expression ax2("(a>(b>c))>((a>b)>(a>c))");
 	Expression ax_swap_rule("(a>!b)>(b>!a)");
 	Expression ax11("a>a");
-	std::vector<Expression> axioms = { ax1, ax2, ax3, ax_swap_rule, ax11 };
+	std::vector<Expression> axioms = { ax1, ax2, ax_swap_rule, ax11 };
 
 	axioms.push_back(modus_ponens(axioms[0], axioms[0]));
 	axioms.push_back(modus_ponens(axioms[2], axioms[0]));
@@ -130,32 +138,45 @@ int main()
 	axioms.push_back(modus_ponens(axioms[8], axioms[10]));
 	axioms.push_back(modus_ponens(axioms[11], axioms[9]));
 
+	std::vector<std::pair<int, int>> order = {
+		{-1, -1},
+		{-1, -1},
+		{-1, -1},
+		{-1, -1},
+		{0, 0},
+		{2, 0},
+		{1, 1},
+		{3, 1},
+		{7, 0},
+		{5, 1},
+		{4, 6},
+		{8, 10},
+		{11, 9},
+	};
+
 	for (std::size_t i = 0; i < axioms.size(); ++i)
 	{
-		std::cout << i << ' ' << axioms[i] << '\n';
-	}
-	*/
-
+		std::cout << i << ' ' << "mp(" << order[i].first << ',' << order[i].second << "): " << axioms[i] << '\n';
+	}*/
 
 	//std::cout << axioms.back() << '\n';
 	//Expression a("a>a");
-	//Expression b("((a>b)>a)>((a>b)>a)");
+	//Expression b("((a>b)>a)>((a>sb)>a)");
 
 	//std::cout << modus_ponens(a,b) << '\n';
 
-	/*std::string expression_str = "!a>!a";
+	/*std::string expression_str = "!a>(a>b)";
 	//std::cin >> expression_str;
 	Expression target(expression_str);
-	//target.make_permanent();
-
-	std::cout << "Your input: " << target << '\n';
+	target.normalize();
+	target.make_permanent();
 
 	Expression ax1("a>(b>a)");
 	Expression ax2("(a>(b>c))>((a>b)>(a>c))");
 	Expression ax3("(!a>!b)>((!a>b)>a)");
-	//Expression ax11("a>a");
-	//Expression ax112("!a>!a");
 	std::vector<Expression> axioms = { ax1, ax2, ax3 };
+
+	std::cerr << "you input: " << target << '\n';
 
 	Solver solve(axioms, target);
 	solve.solve();
@@ -163,3 +184,20 @@ int main()
 	std::cout << solve.thought_chain() << '\n';*/
 	return 0;
 }
+
+/*
+P->(Q->P);                      # 1;
+(P->(Q->R))->((P->Q)->(P->R));  # 2;
+(!P->!Q)->((!P->Q)->P); # 3;
+!(P->!Q)->P; # 4;
+!(P->!Q)->Q; # 5;
+!P->(P->!Q); # 4inv;
+!Q->(P->!Q); # 5inv;
+P->(Q->!(P->!Q)); # 6;
+(!P->!Q)->(Q->P); # isr;
+P->(!P->Q); # 7;
+Q->(!P->Q); # 8;
+!P->(P->Q); # 10;
+P->P; # 11;
+!P->!P; # 11 real;
+*/
